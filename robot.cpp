95db6fe4fbb6e02377bca3e4926a905dc871ab35
raphiola <70310203+raphiola@users.noreply.github.com>
2022-03-19 00:45:59 +0700

    Buat bagian play
    masih perlu perbaikan

--- robot.cpp
+++ robot.cpp
@@ -1,4 +1,4 @@
- #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <conio.h>
 #include <windows.h>
 #include "axis.h"
@@ -9,8 +9,8 @@ using namespace std;
 int enemyY[1];
 int enemyX[1];
 int enemyFlag[1];
-char robot[2][2]={'*','*',
-					'*','*'};
+//char robot[2][2]={'*','*',
+					//'*','*'};
 int health = 100;
 
 axis make_axis (int x, int y)
@@ -20,7 +20,6 @@ axis make_axis (int x, int y)
 	a.y = y;
 	return a;
 }
-
 void move_axis (axis* a, char c)
 {
 	if (c == 'w')
@@ -46,13 +45,12 @@ void move_axis (axis* a, char c)
 		}
 	}
 }
-
 void gotoxy(int x, int y)
 {
 	COORD coord;
 	coord.X = x;
 	coord.Y = y;
-	
+
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord); 
 }
 /*void genEnemy(int ind){
@@ -83,6 +81,44 @@ double distance(axis* a, axis* b)
 	ret = sqrt(ret);
 	return ret;
 }
+
+robot::robot()
+{
+	this->health = 50;
+	this->power = 5;
+	this->range = 5;
+	this->position.x = 0;
+	this->position.y = 0;
+}
+
+robot::robot(char k, axis a)
+{
+	if (k == 'r')
+	{
+		this->health = 50;
+		this->power = 5;
+		this->range = 5;
+	}
+	else
+	{
+		this->health = 15;
+		this->power = 1;
+		this->range = 40;
+	}
+	this->position.x =a.x;
+	this->position.y =a.y;
+}
+
+void robot::moverobot(axis a)
+{
+	this->position = a;
+}
+
+bool robot::isalive()
+{
+	return (health > 0);
+}
+
 void gameover(){
 	system("cls");
 	cout<<endl;
@@ -96,7 +132,7 @@ void updateHealth(){
 	//gotoxy(7, 5);cout<<"Score: "<<health<<endl;
 }
 void instructions(){
-	
+
 	system("cls");
 	cout<<"Instructions";
 	cout<<"\n----------------";
@@ -110,16 +146,73 @@ void instructions(){
 	getch();
 }
 void play(){
-	//
+	system("cls");
+	char c;
+	robot r;
+	int cnt = 0;
+	for (int i = 0; i < 20; i++)
+	{
+		printf("|");
+		if (i != 19)
+		{
+			printf("\n");
+		}
+	}
+	for (int i = 0; i < 20; i++)
+	{
+		printf("_");
+	}
+	axis a = make_axis (0,0);
+	while (r.isalive())
+	{
+		srand((unsigned) time(0));
+		axis t = make_axis (rand()%20,rand()%20);
+		robot k('k',t);
+		gotoxy(t.x,19-t.y);
+		printf(".");
+		while (k.isalive())
+		{
+			c = getch();
+			if (c == ' ')
+			{
+				if (distance(&a,&t) <= r.range)
+				{
+					k.health-=r.power;
+				}
+				else
+				{
+					gotoxy(0,20);
+					printf("out of range");
+					gotoxy(a.x,19-a.y);
+				}
+			}
+			else
+			{
+				move_axis (&a,c);
+				gotoxy(a.x,19-a.y);
+			}
+			if (distance(&a,&t) <= k.range)
+			{
+				r.health-=k.power;
+				if (!r.isalive())
+				{
+					break;
+				}
+			}
+		}
+		if (!k.isalive())
+		{
+			cnt++;
+		}
+	}
+	gotoxy(0,20);
+	printf("Successfully destroy %d mecha kurama\n",cnt);
+	printf("Press any key to go back to menu");
+	getch();
 }
 
 int main()
 {
-	char c;
-	axis a = make_axis (0,0);
-	srand((unsigned) time(0));
-	axis t = make_axis (rand()%20,rand()%20);
-
 	do{
 		system("cls");
 		gotoxy(10,5); cout<<" -------------------------- "; 
@@ -130,21 +223,11 @@ int main()
 		gotoxy(10,11); cout<<"3. Quit";
 		gotoxy(10,13); cout<<"Select option: ";
 		char op = getche();
-		
+
 		if( op=='1') play();
 		else if( op=='2') instructions();
 		else if( op=='3') exit(0);
-		
+
 	}while(1);
-	gotoxy(t.x,19-t.y);
-	printf(".");
-	gotoxy(0,20);
-	printf("%d, %d\n", t.x, t.y);
-	while (true)
-	{
-		c = getch();
-		move_axis (&a,c);
-		gotoxy(a.x,19-a.y);
-	}
     return 0;
 }
